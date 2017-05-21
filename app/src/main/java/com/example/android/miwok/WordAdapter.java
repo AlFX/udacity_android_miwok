df5d63fe12f0ac75236c2ab0f6021849717c58bf
AlFX <alessio.fanciulli.oxilia@gmail.com>
2017-05-21 16:30:00 +0100

    initial commit

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,45 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    private static final String LOG_TAG = WordAdapter.class.getSimpleName();
+
+    public WordAdapter (Activity context, ArrayList<Word> Words){
+        super(context, 0, Words);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        Word currentWord = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        return listItemView;
+    }
+}
+
+// this mess is explained here
+//      https://www.youtube.com/watch?v=ec9YBW3OTpY
+//      https://youtu.be/C4zSpiZPyXU
